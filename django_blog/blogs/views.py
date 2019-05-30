7ddb4822602efd824ff72632172689b7d1ad4a76
Takuya <takuya.miraidenshi@gmail.com>
2019-05-30 12:17:26 +0900

    add: pagination

--- django_blog/blogs/views.py
+++ django_blog/blogs/views.py
@@ -1,9 +1,19 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Blog
 from .forms import PhotoForm
 
 def index(request):
-    blogs = Blog.objects.order_by('-created_datetime')
+    blog_list = Blog.objects.order_by('-created_datetime')
+    page = request.GET.get('page',1)
+
+    paginator = Paginator(blog_list,1)
+    try:
+        blogs = paginator.page(page)
+    except PageNotAnInteger:
+        blogs = paginator.page(1)
+    except EmptyPage:
+        blogs = paginator.page(paginator.num_pages)
     return render(request, 'blogs/index.html', {'blogs': blogs})
 
     if request.method == 'GET':
